03ef45c65bafb87ddac9018e6f4bb7d63693b09a
con111 <69613112+con111@users.noreply.github.com>
2020-08-13 00:51:11 -0700

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,10 @@
+public class Hello2 {
+    public static void main(String[] args){
+        System.out.println("Hello World ");
+        System.out.println("My favorite ice cream flavor is cookies and cream.");
+        System.out.println("My favorite board game is Shadows Over Camelot.");
+        System.out.println("My favorite color is blue.");
+        System.out.println("My most likely major will be statistics.");
+        System.out.println("I hate high temperatures.");
+    }
+}
